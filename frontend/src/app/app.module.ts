d9c45456f092fc2ff981ae61f50337397327e612
AndriiMykh <bob@example.com>
2020-09-27 18:26:17 +0200

    Added header, footer, navigation

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import {Routes,RouterModule} from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
+const routes:Routes=[
+  {path: 'flowers', component:WelcomeComponent},
+  {path: '', redirectTo:'/flowers',pathMatch:'full'},
+  {path: '**', redirectTo:'/flowers',pathMatch:'full'}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     AppRoutingModule
   ],
